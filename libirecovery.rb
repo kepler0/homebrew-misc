229ae36a678758f08b06c192c09215279fbb4c11
Kepler Sticka-Jones <kbsj269@gmail.com>
2014-03-12 21:33:44 -0600

    Update libirecovery.rb

--- libirecovery.rb
+++ libirecovery.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class libirecovery < Formula
+class Libirecovery < Formula
   homepage 'https://github.com/Chronic-Dev/libirecovery.git'
   head 'https://github.com/Chronic-Dev/libirecovery.git', :branch => 'master'
   url 'https://github.com/Chronic-Dev/libirecovery/archive/8d2df80b4aac1b8d5d139b1fca6046fd62878d58.zip'
