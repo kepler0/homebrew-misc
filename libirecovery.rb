b812444526e10700c9db37e4ec9a9a093e0f400f
Kepler Sticka-Jones <kbsj269@gmail.com>
2014-03-12 21:34:38 -0600

    Update libirecovery.rb

--- libirecovery.rb
+++ libirecovery.rb
@@ -4,7 +4,7 @@ class Libirecovery < Formula
   homepage 'https://github.com/Chronic-Dev/libirecovery.git'
   head 'https://github.com/Chronic-Dev/libirecovery.git', :branch => 'master'
   url 'https://github.com/Chronic-Dev/libirecovery/archive/8d2df80b4aac1b8d5d139b1fca6046fd62878d58.zip'
-  sha1 '8d2df80b4aac1b8d5d139b1fca6046fd62878d58'
+  sha1 '90c1393b927d4e90b893befd17d33611dffbbce2'
   
   depends_on 'autoconf' => :build
   depends_on 'automake' => :build
