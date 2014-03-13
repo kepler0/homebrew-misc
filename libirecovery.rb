97aa68b79589d24cfb00991e83c8535f8cd95df8
Kepler Sticka-Jones <kbsj269@gmail.com>
2014-03-12 21:32:51 -0600

    Could work

--- libirecovery.rb
+++ libirecovery.rb
@@ -5,4 +5,13 @@ class libirecovery < Formula
   head 'https://github.com/Chronic-Dev/libirecovery.git', :branch => 'master'
   url 'https://github.com/Chronic-Dev/libirecovery/archive/8d2df80b4aac1b8d5d139b1fca6046fd62878d58.zip'
   sha1 '8d2df80b4aac1b8d5d139b1fca6046fd62878d58'
+  
+  depends_on 'autoconf' => :build
+  depends_on 'automake' => :build
+  
+  def install
+    system "./autogen.sh"
+    system "make"
+  end
+  
 end
